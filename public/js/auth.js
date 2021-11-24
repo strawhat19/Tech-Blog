3cc29f6a2b00c948f4dedb03f5895c27e8fd0698
Rakib Ahmed <rakib987@gmail.com>
2021-11-24 16:28:03 -0500

    update and deploy to heroku

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,53 +1,53 @@
-$(`#login-form`).bootstrapValidator();
+// $(`#login-form`).bootstrapValidator();
 
-$(`#login-form`).submit(async (evt) => {
-    evt.preventDefault();
-    const email = document.querySelector(`#user-email`).value
-    const password = document.querySelector(`#user-password`).value
+// $(`#login-form`).submit(async (evt) => {
+//     evt.preventDefault();
+//     const email = document.querySelector(`#user-email`).value
+//     const password = document.querySelector(`#user-password`).value
 
-    try {
-       const response = await fetch(`/api/login`, {
-        method: `POST`,
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-        })
+//     try {
+//        const response = await fetch(`/api/login`, {
+//         method: `POST`,
+//         body: JSON.stringify({ email, password }),
+//         headers: { 'Content-Type': 'application/json' },
+//         })
 
-        if (response.ok) {
-            console.log(response);
-            document.location.replace(`/dashboard`)
-        } else {
-            console.log(response)
-            window.alert(response.statusText)
-        }
-    } catch (e) {
-        window.alert(`Login Failed`)
-    }
-});
+//         if (response.ok) {
+//             console.log(response);
+//             document.location.replace(`/dashboard`)
+//         } else {
+//             console.log(response)
+//             window.alert(response.statusText)
+//         }
+//     } catch (e) {
+//         window.alert(`Login Failed`)
+//     }
+// });
 
-async function signupFormHandler(event) {
-    event.preventDefault();
+// async function signupFormHandler(event) {
+//     event.preventDefault();
 
-    const username = document.querySelector(`#user-user-name`).value.trim();
-    const email = document.querySelector(`#user-email`).value.trim();
-    const password = document.querySelector(`#user-password`).value.trim();
+//     const username = document.querySelector(`#user-user-name`).value.trim();
+//     const email = document.querySelector(`#user-email`).value.trim();
+//     const password = document.querySelector(`#user-password`).value.trim();
 
-    if (username && email && password) {
-        const response = await fetch(`/api/register`, {
-            method: `post`,
-            body: JSON.stringify({
-                email,
-                password,
-                username,
-            }),
-            headers: {'Content-Type': 'application/json'}
-        });
-        if (response.ok) {
-            alert(`Account created! Logging you in now.`);
-            document.location.replace(`/dashboard`);
-        } else {
-            alert(response.statusText)
-        }
-    }
-}
+//     if (username && email && password) {
+//         const response = await fetch(`/api/register`, {
+//             method: `post`,
+//             body: JSON.stringify({
+//                 email,
+//                 password,
+//                 username,
+//             }),
+//             headers: {'Content-Type': 'application/json'}
+//         });
+//         if (response.ok) {
+//             alert(`Account created! Logging you in now.`);
+//             document.location.replace(`/dashboard`);
+//         } else {
+//             alert(response.statusText)
+//         }
+//     }
+// }
 
-document.querySelector(`.signup-form`).addEventListener(`submit`, signupFormHandler);
+// document.querySelector(`.signup-form`).addEventListener(`submit`, signupFormHandler);
--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 const path = require(`path`);
 const express = require(`express`);
-const exphbs = require(`express-handlebars`);
-const routes = require(`./controllers`);
-const session = require(`express-session`);
-const sequelize = require(`./config/connection`);
-const SequelizeStore = require(`connect-session-sequelize`)(session.Store);
-
-// const sess = {
-//     secret: `Secret Secret`,
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// };
+// const exphbs = require(`express-handlebars`);
+// const session = require(`express-session`);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// app.engine(`.hbs`, exphbs({ extname: `.hbs` }));
-// app.set(`view engine`, `.hbs`);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,10 +14,4 @@ app.get('/', (req,res) => {
     res.send(`Hello World`)
 })
 
-// app.use(session(sess));
-
-// app.use(routes);
-
-// sequelize.sync({ force: false }).then(() => {
-//     app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}/`));
-// });
+app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}/`));
