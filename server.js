1586f5b5dba3fa720b74959c20c0a44c5f5f2f8f
Rakib Ahmed <rakib987@gmail.com>
2021-11-24 16:39:29 -0500

    update test

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const path = require(`path`);
 const express = require(`express`);
-// const exphbs = require(`express-handlebars`);
-// const session = require(`express-session`);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,8 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, `public`)));
 
-app.get('/', (req,res) => {
-    res.send(`Hello World`)
+app.get(`/`, (req,res) => {
+    res.send(`Hello Guys 2`)
 })
 
 app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}/`));
